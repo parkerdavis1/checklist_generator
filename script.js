17386cf13a1396f794d869a1473cecd2b740650f
Parker <parkied@gmail.com>
2021-10-11 14:00:02 -0700

    First pass at random checklist array

---
+++ script.js
@@ -0,0 +1,66 @@
+//maybe change to object oriented?
+const desertBirdsArray = [
+  "Curve-billed Thrasher",
+  "Verdin",
+  "Black-tailed Gnatcatcher",
+  "Cactus Wren",
+  "Abert's Towhee",
+  "House Finch",
+  "Mourning Dove",
+  "White-winged Dove",
+  "Gila Woodpecker"
+];
+const forestBirdsArray = [
+  "Dark-eyed Junco",
+  "Steller's Jay",
+  "Northern Flicker",
+  "Mountain Chickadee",
+  "Western Bluebird",
+  "Pygmy Nuthatch",
+  "White-breasted Nuthatch",
+  "Red-breasted Nuthatch",
+  "Yellow-rumped Warbler",
+  "Ruby-crowned Kinglet"
+];
+const wetlandsBirdsArray = [
+  "Mallard",
+  "Green-winged Teal",
+  "Black-necked Stilt",
+  "American Avocet",
+  "Neotropic Cormorant",
+  "Double-crested Cormorant",
+  "American Widgeon",
+  "Canada Goose",
+  "Belted Kingfisher",
+  "Black-capped Night-heron"
+];
+//const habitatArray = [desertBirdsArray, forestBirdsArray, wetlandsBirdsArray];
+
+const randomNumberOfSpecies = arr => Math.floor(Math.random() * arr.length);
+
+const chooseRandomBird = arr => arr[Math.floor(Math.random() * arr.length)];
+
+let randomNumberOfEachSpecies = () => Math.floor(Math.random() * 50);
+
+const createChecklistSpeciesArray = arr => {
+    let numOfBirds = randomNumberOfSpecies(arr);
+    let checklistArray = [];
+    while (checklistArray.length < numOfBirds) {
+        let bird = chooseRandomBird(arr);
+        if (!checklistArray.includes(bird)) {
+            checklistArray.push(bird);
+        }
+    }
+    return checklistArray;
+};
+
+const createChecklistArray = speciesArray => {
+    let checklist = [];
+    for (let species of speciesArray) {
+        checklist.push(randomNumberOfEachSpecies() + ' ' + species);
+    }
+    return checklist;
+};
+
+
+console.log(createChecklistArray(createChecklistSpeciesArray(desertBirdsArray)));
