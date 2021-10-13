5cc86b896374a7039dedcc51ac9e1ab69571fbea
Parker <parkied@gmail.com>
2021-10-12 19:53:21 -0700

    New console display, slightly expanded bird list

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//maybe change to object oriented?
 const birdlists = {
   desert: [
     "Curve-billed Thrasher",
@@ -9,7 +8,15 @@ const birdlists = {
     "House Finch",
     "Mourning Dove",
     "White-winged Dove",
-    "Gila Woodpecker"
+    "Gila Woodpecker",
+    "Anna's Hummingbird",
+    "Gambel's Quail",
+    "Inca Dove",
+    "Lesser Goldfinch",
+    "Costa's Hummingbird",
+    "Rock Wren",
+    "Greater Roadrunner",
+    "Common Raven"
   ],
   forest: [
     "Dark-eyed Junco",
@@ -21,7 +28,14 @@ const birdlists = {
     "White-breasted Nuthatch",
     "Red-breasted Nuthatch",
     "Yellow-rumped Warbler",
-    "Ruby-crowned Kinglet"
+    "Ruby-crowned Kinglet",
+    "Red-faced Warbler",
+    "Lesser Goldfinch",
+    "Pine Siskin",
+    "American Robin",
+    "Acorn Woodpecker",
+    "Townsend's Solitaire",
+    "Brown Creeper"
   ],
   wetlands: [
     "Mallard",
@@ -33,7 +47,19 @@ const birdlists = {
     "American Widgeon",
     "Canada Goose",
     "Belted Kingfisher",
-    "Black-crowned Night-heron"
+    "Black-crowned Night-heron",
+    "Ring-necked Duck",
+    "Ruddy Duck",
+    "Yellow-headed Blackbird",
+    "Common Loon",
+    "Clark's Grebe",
+    "American White Pelican",
+    "Killdeer",
+    "Least Sandpiper",
+    "Wilson's Snipe",
+    "Great Blue Heron", 
+    "Great Egret",
+    "Least Bittern"
   ]
 }
 
@@ -62,16 +88,19 @@ const createChecklistSpeciesArray = array => {
 const createChecklistArray = speciesArray => {
     let checklist = [];
     for (let species of speciesArray) {
-        checklist.push(getRandom(15, 1) + ' ' + species);
+        checklist.push(getRandom(10, 1) + ' ' + species);
     }
     return checklist;
 };
 
 const displayChecklist = habitat => {
-  console.log(`You went to the ${habitat} and saw:`);
+  console.log(`\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\nYou went to the ${habitat} and saw: \n`);
   let listDisplay = createChecklistArray(createChecklistSpeciesArray(birdlists[habitat]));
   console.log(listDisplay.join('\n'));
+  console.log('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n')
   //console.log(createChecklistArray(createChecklistSpeciesArray(birdlists[habitat])));
 }
 
 displayChecklist('desert');
+
+//add functionality to randomly select a habitat
